642bbef840c3b2108ad934a3192f4e305a326e02
PeterDavid3800 <97628044+PeterDavid3800@users.noreply.github.com>
2026-03-23 09:49:44 +0300

    Admin Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import VisitForm from "./components/VisitForm";
 import AdminDashboard from "./components/AdminDashboard";
 
